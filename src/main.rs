59181d751e65e1d8b874df9636762fe1f27934cc
Lupowilk <105706387+Lupowilk@users.noreply.github.com>
2025-06-11 09:20:55 +0200

    Added first Axum web server with basic endpoint as the foudnation for the blockchain APIs

--- src/main.rs
+++ src/main.rs
@@ -6,45 +6,26 @@ use crate::models::Block;
 use crate::models::Blockchain;
 use crate::models::Transaction;
 
-fn main() {
-    print!("Testing Transaction model...");
+use axum::serve;
+use axum::{
+    Json, Router,
+    http::StatusCode,
+    routing::{get, post},
+};
+use serde::{Deserialize, Serialize};
 
-    let tx = Transaction {
-        tx_id: 1,
-        sender: "0x1234".to_string(),
-        receiver: "0x456def".to_string(),
-        amount: 1000,
-        timestamp: 12345567,
-    };
+async fn root() -> String {
+    "Hello Blockchian API".to_string()
+}
 
-    let tx2 = Transaction {
-        tx_id: 2,
-        sender: "0x2222".to_string(),
-        receiver: "0x222def".to_string(),
-        amount: 2000,
-        timestamp: 22222222,
-    };
+#[tokio::main]
+async fn main() {
+    // Router
+    let user_request = Router::new().route("/", get(root));
 
-    let tx_vec = vec![tx, tx2];
+    let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
 
-    let block = Block {
-        block_id: 1,
-        previous_block_hash: "zxd123".to_string(),
-        timestamp: 123,
-        tx_list: tx_vec,
-        block_hash: "hdhh1234".to_string(),
-    };
-
-    println!("Block created: {:?}", block);
-    println!("Blockchain API is ready!");
-
-    let blockchain = Blockchain {
-        chain_of_blocks: vec![block],
-        total_tx: 2,
-        metadata: "I love Rust".to_string(),
-        current_block_height: 1,
-    };
-
-    println!("Block created: {:?}", blockchain);
-    println!("Blockchain is up and running!");
+    //Start the server
+    println!("Server starting on http://localhost:3000");
+    axum::serve(listener, user_request).await.unwrap();
 }
