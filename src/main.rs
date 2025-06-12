599fa0e246f27f793554aa1cd1469e732e9a2c84
Lupowilk <105706387+Lupowilk@users.noreply.github.com>
2025-06-12 09:36:10 +0200

    Add REST Api for blochchain transactions

--- src/main.rs
+++ src/main.rs
@@ -2,26 +2,63 @@ mod database;
 mod handlers;
 mod models;
 
-use crate::models::Block;
-use crate::models::Blockchain;
 use crate::models::Transaction;
-
-use axum::serve;
 use axum::{
     Json, Router,
-    http::StatusCode,
     routing::{get, post},
 };
-use serde::{Deserialize, Serialize};
 
-async fn root() -> String {
-    "Hello Blockchian API".to_string()
+use serde::{Deserialize, Serialize};
+use serde_json::json;
+
+async fn root() -> Json<serde_json::Value> {
+    Json(json!(
+        {
+      "message": "Hello Blockchain API!",
+      "status": "running",
+      "version": "1.0"
+    }
+    ))
+}
+
+async fn get_transactions() -> Json<serde_json::Value> {
+    // Create sample transactions
+    let tx1 = Transaction {
+        tx_id: 1,
+        sender: "0x123abc".to_string(),
+        receiver: "0x456def".to_string(),
+        amount: 1000,
+        timestamp: 1234567890,
+    };
+
+    let tx2 = Transaction {
+        tx_id: 2,
+        sender: "0x789ghi".to_string(),
+        receiver: "0x012jkl".to_string(),
+        amount: 2500,
+        timestamp: 1234567900,
+    };
+
+    Json(json!( {
+        "transactions": [tx1, tx2],
+        "count": 2
+    }))
+}
+
+async fn create_transaction(Json(payload): Json<Transaction>) -> Json<serde_json::Value> {
+    Json(json!({
+        "message":"Transaction created successfully",
+        "transaction": payload
+    }))
 }
 
 #[tokio::main]
 async fn main() {
     // Router
-    let user_request = Router::new().route("/", get(root));
+    let user_request = Router::new()
+        .route("/", get(root))
+        .route("/transactions", get(get_transactions))
+        .route("/transactions", post(create_transaction));
 
     let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
 
